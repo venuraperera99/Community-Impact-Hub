1d198424359c317b3bdb71e5250b2c937400162e
Venura <venura.perera1999@gmail.com>
2024-02-09 10:18:07 -0500

    created frontend for contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,11 +1,19 @@
-// About.js
+// Contact.js
+
 import React from 'react';
 import './Contact.css'; // Import the CSS file for styling
 
 const Contact = () => {
   return (
-    <div className='outer-container'>
-      <p>Hello</p>
+    <div className='outer'>
+      <div className="form-container">
+        <h1>Contact</h1>
+        <input type="text" placeholder="Full Name" className="input-field" />
+        <input type="email" placeholder="Email Address" className="input-field" />
+        <input type="text" placeholder="Phone Number" className="input-field" />
+        <textarea placeholder="Have anything to say?" className="message-field"></textarea>
+        <button className="send-button">Send</button>
+      </div>
     </div>
   );
 }
