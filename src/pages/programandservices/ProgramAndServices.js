d7b2da882a264cb202d9efc37aaf1d1f9c06defd
Venura <venura.perera1999@gmail.com>
2024-03-23 20:25:39 -0400

    Added Strapi integration to ProgramAndServices

--- src/pages/programandservices/ProgramAndServices.js
+++ src/pages/programandservices/ProgramAndServices.js
@@ -1,6 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "./ProgramAndServices.css";
-//import Carousel from '../../components/carousel/Carousel';
 
 const items = [
     {
@@ -23,6 +22,22 @@ const items = [
 ];
 
 const ProgramAndServices = () => {
+    const [programs, setPrograms] = useState([]);
+
+    useEffect(() => {
+        fetchPrograms();
+    }, []);
+
+    const fetchPrograms = async () => {
+        try {
+            const response = await fetch('http://localhost:1337/api/program-and-service?populate=*'); // Adjust URL to your Strapi endpoint
+            const data = await response.json();
+            console.log(data)
+            setPrograms(data.data.attributes.CardInfo);
+        } catch (error) {
+            console.error('Error fetching programs:', error);
+        }
+    };
 
     const [selectedCard, setSelectedCard] = useState(null);
 
@@ -35,20 +50,25 @@ const ProgramAndServices = () => {
             <div className='content'>
                 <h1>Programs & Services</h1>
             </div>
-            {/*<Carousel items={items} />*/}
-            <div className="card-container">
-                {items.map((item, index) => (
-                <div
-                key={index}
-                className={`card ${selectedCard === index ? 'selected' : ''}`}
-                onClick={() => handleCardClick(index)}>
-                    <h3>{item.title}</h3>
-                    <p>{item.description}</p>
-                </div>
+            {programs ? 
+            <>
+                <div className="card-container">
+                    {programs.map((program, index) => (
+                    <div
+                        key={index}
+                        className={`card ${selectedCard === index ? 'selected' : ''}`}
+                        onClick={() => handleCardClick(index)}
+                    >
+                        <h3>{program.title}</h3>
+                        <p>{program.description[0].children[0].text}</p>
+                    </div>
                 ))}
-            </div>
+                </div>
+            </> :
+                <p>Loading...</p>
+            }
+            
         </div>
     )
 }
-
-export default ProgramAndServices
+export default ProgramAndServices;
