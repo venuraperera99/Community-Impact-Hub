14c23dcad6d682858e62c7ced31a3b0fa2735c07
Venura <venura.perera1999@gmail.com>
2024-03-25 18:11:02 -0400

    Implemented Strapi for Header and added French translation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { Link } from 'react-router-dom'; 
 import './Header.css';
 import { FaUser } from 'react-icons/fa';
 import Image from "../../images/Logo.png";
 import LanguagePicker from '../languagepicker/LanguagePicker';
+import LanguageContext from '../../contexts/LanguageContext/LanguageContext';
 
 export const Header = () => {
+  const [headerData, setHeaderData] = useState(null);
+  const { selectedLanguage } = useContext(LanguageContext);
+
+  useEffect(() => {
+    fetchHeaderData();
+  }, [selectedLanguage]);
+
+  const fetchHeaderData = async () => {
+    if(selectedLanguage === "English"){
+      try {
+        const response = await fetch('http://localhost:1337/api/header');
+        const data = await response.json();
+        console.log(selectedLanguage)
+        setHeaderData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    } else if (selectedLanguage === "French"){
+      try {
+        const response = await fetch('http://localhost:1337/api/header-french');
+        const data = await response.json();
+        console.log(selectedLanguage)
+        setHeaderData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    }
+    
+  };
+
   return (
     <div className="header-container">
         <img src={Image} alt="Community Impact Hub" style={{  width: '200px', height: 'auto%' }} />
       <nav>
+        
         <ul className="header-tabs">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/resources">Resources</Link></li>
-          <li><Link to="/programs">Programs & Services</Link></li>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-          <li><Link to="/summercamp">Summer Camp Program</Link></li>
+          <li><Link to="/">{headerData.headerLink1}</Link></li>
+          <li><Link to="/about">{headerData.headerLink2}</Link></li>
+          <li><Link to="/resources">{headerData.headerLink3}</Link></li>
+          <li><Link to="/programs">{headerData.headerLink4}</Link></li>
+          <li><Link to="/register">{headerData.headerLink5}</Link></li>
+          <li><Link to="/contact">{headerData.headerLink6}</Link></li>
+          <li><Link to="/summercamp">{headerData.headerLink7}</Link></li>
         </ul>
       </nav>
 
       <nav >
         <ul>
-          <li><FaUser size={24} color={'black'} /></li>
+          <li><FaUser size={24} color={'black'} />{headerData.signIn}</li>
           <li><LanguagePicker/></li>
         </ul>
       </nav>
