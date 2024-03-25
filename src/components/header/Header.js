bbbac531a397c67c526fc8a39a626f4e5e9df551
Venura <venura.perera1999@gmail.com>
2024-03-25 19:06:57 -0400

    Fixed Strapi Bug in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -19,7 +19,6 @@ export const Header = () => {
       try {
         const response = await fetch('http://localhost:1337/api/header');
         const data = await response.json();
-        console.log(selectedLanguage)
         setHeaderData(data.data.attributes);
       } catch (error) {
         console.error('Error fetching about data:', error);
@@ -28,7 +27,6 @@ export const Header = () => {
       try {
         const response = await fetch('http://localhost:1337/api/header-french');
         const data = await response.json();
-        console.log(selectedLanguage)
         setHeaderData(data.data.attributes);
       } catch (error) {
         console.error('Error fetching about data:', error);
@@ -40,25 +38,29 @@ export const Header = () => {
   return (
     <div className="header-container">
         <img src={Image} alt="Community Impact Hub" style={{  width: '200px', height: 'auto%' }} />
-      <nav>
-        
-        <ul className="header-tabs">
-          <li><Link to="/">{headerData.headerLink1}</Link></li>
-          <li><Link to="/about">{headerData.headerLink2}</Link></li>
-          <li><Link to="/resources">{headerData.headerLink3}</Link></li>
-          <li><Link to="/programs">{headerData.headerLink4}</Link></li>
-          <li><Link to="/register">{headerData.headerLink5}</Link></li>
-          <li><Link to="/contact">{headerData.headerLink6}</Link></li>
-          <li><Link to="/summercamp">{headerData.headerLink7}</Link></li>
-        </ul>
-      </nav>
+        {headerData ? 
+        <>
+          <nav>
+            <ul className="header-tabs">
+              <li><Link to="/">{headerData.headerLink1}</Link></li>
+              <li><Link to="/about">{headerData.headerLink2}</Link></li>
+              <li><Link to="/resources">{headerData.headerLink3}</Link></li>
+              <li><Link to="/programs">{headerData.headerLink4}</Link></li>
+              <li><Link to="/register">{headerData.headerLink5}</Link></li>
+              <li><Link to="/contact">{headerData.headerLink6}</Link></li>
+              <li><Link to="/summercamp">{headerData.headerLink7}</Link></li>
+            </ul>
+          </nav>
 
-      <nav >
-        <ul>
-          <li><FaUser size={24} color={'black'} />{headerData.signIn}</li>
-          <li><LanguagePicker/></li>
-        </ul>
-      </nav>
+          <nav >
+            <ul>
+              <li><FaUser size={24} color={'black'} />{headerData.signIn}</li>
+              <li><LanguagePicker/></li>
+            </ul>
+          </nav>
+        </> : 
+        <p>Loading...</p>}
+      
     </div>
   );
 };
