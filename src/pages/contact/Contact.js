23c033951b4dd3c37a4d3e8d584b912588788ef5
Venura <venura.perera1999@gmail.com>
2024-03-25 20:23:26 -0400

    Implemented Strapi to Contact us page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import './Contact.css';
+import LanguageContext from '../../contexts/LanguageContext/LanguageContext';
+
 
 const Contact = () => {
+
+  const [contactData, setContactData] = useState(null);
+  const { selectedLanguage } = useContext(LanguageContext);
+
+  useEffect(() => {
+    fetchContactData();
+  }, [selectedLanguage]);
+
+  const fetchContactData = async () => {
+    if(selectedLanguage === "English"){
+      try {
+        const response = await fetch('http://localhost:1337/api/contact-us');
+        const data = await response.json();
+        setContactData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    } else if (selectedLanguage === "French"){
+      try {
+        const response = await fetch('http://localhost:1337/api/contact-us-french');
+        const data = await response.json();
+        setContactData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    }
+    
+  };
+
   // Define validation schema using Yup
   const validationSchema = Yup.object().shape({
     fullName: Yup.string().required('Full Name is required'),
@@ -46,38 +77,42 @@ const Contact = () => {
 
   return (
     <div className='outer-contact'>
-      <div className="form-container">
-        <h1>Contact Us</h1>
-        {/* Formik wrapper for the form */}
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          {({ errors, touched }) => (
-            <Form>
-              {/* Full Name field */}
-              <Field type="text" name="fullName" placeholder="Full Name" className="input-field" />
-              <ErrorMessage name="fullName" component="div" className="error-message" />
+      {contactData ?
+      <>
+        <div className="form-container">
+          <h1>{contactData.formTitle}</h1>
+          {/* Formik wrapper for the form */}
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                {/* Full Name field */}
+                <Field type="text" name="fullName" placeholder={contactData.namePlaceholder} className="input-field" />
+                <ErrorMessage name="fullName" component="div" className="error-message" />
 
-              {/* Email field */}
-              <Field type="email" name="email" placeholder="Email Address" className="input-field" />
-              <ErrorMessage name="email" component="div" className="error-message" />
+                {/* Email field */}
+                <Field type="email" name="email" placeholder={contactData.emailPlaceholder} className="input-field" />
+                <ErrorMessage name="email" component="div" className="error-message" />
 
-              {/* Phone Number field */}
-              <Field type="text" name="phoneNumber" placeholder="Phone Number" className="input-field" />
-              <ErrorMessage name="phoneNumber" component="div" className="error-message" />
+                {/* Phone Number field */}
+                <Field type="text" name="phoneNumber" placeholder={contactData.phonePlaceholder} className="input-field" />
+                <ErrorMessage name="phoneNumber" component="div" className="error-message" />
 
-              {/* Message textarea */}
-              <Field as="textarea" name="message" placeholder="Have anything to say?" className="message-field" />
-              <ErrorMessage name="message" component="div" className="error-message" />
-              
-              {/* Submit button */}
-              <button type="submit" className="send-button">Send</button>
-            </Form>
-          )}
-        </Formik>
-      </div>
+                {/* Message textarea */}
+                <Field as="textarea" name="message" placeholder={contactData.messagePlaceholder} className="message-field" />
+                <ErrorMessage name="message" component="div" className="error-message" />
+                
+                {/* Submit button */}
+                <button type="submit" className="send-button">{contactData.submitButton}</button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </> :
+      <p className='outer-contact'>Loading...</p>}
     </div>
   );
 }
