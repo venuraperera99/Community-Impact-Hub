4c25cd23e8fb3e2b166bfe3f499fa8d159b173ac
Venura <venura.perera1999@gmail.com>
2024-03-26 17:28:50 -0400

    Added translation and strapi to page not found

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import "./PageNotFound.css"
+import LanguageContext from '../../contexts/LanguageContext/LanguageContext';
+
 
 export const PageNotFound = () => {
+  const [pageNotFoundData, setPageNotFoundData] = useState(null);
+  const { selectedLanguage } = useContext(LanguageContext);
+
+  useEffect(() => {
+    fetchPageNotFoundData();
+  }, [selectedLanguage]);
+
+  const fetchPageNotFoundData = async () => {
+    if(selectedLanguage === "English"){
+      try {
+        const response = await fetch('http://localhost:1337/api/page-not-found');
+        const data = await response.json();
+        setPageNotFoundData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    } else if (selectedLanguage === "French"){
+      try {
+        const response = await fetch('http://localhost:1337/api/page-not-found-french');
+        const data = await response.json();
+        setPageNotFoundData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    }
+    
+  };
+
   return (
     <div className="not-found-container">
-      <div className="not-found-content">
-        <h1 className='errorh1'>404</h1>
-        <p>Page Not Found</p>
-        <p>Oops! The page you are looking for does not exist.</p>
-      </div>
+      {pageNotFoundData ? 
+      <>
+        <div className="not-found-content">
+          <h1 className='errorh1'>{pageNotFoundData.error}</h1>
+          <p>{pageNotFoundData.textOne}</p>
+          <p>{pageNotFoundData.textTwo}</p>
+        </div>
+      </> :
+      <p>Loading...</p>}
+      
     </div>
   )
 }
