e5c3c6d56d1191d9bca14b8d9a2d1dca5eb58a2c
Venura <venura.perera1999@gmail.com>
2024-03-28 18:55:28 -0400

    Implemented Strapi to Resources

--- src/pages/resources/Resources.js
+++ src/pages/resources/Resources.js
@@ -1,62 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import './Resources.css';
+import LanguageContext from '../../contexts/LanguageContext/LanguageContext';
+
 
 const Resources = () => {
+  const [resourceData, setResourceData] = useState(null);
+  const { selectedLanguage } = useContext(LanguageContext);
+
+  useEffect(() => {
+    fetchResourceData();
+  }, [selectedLanguage]);
+
+  const fetchResourceData = async () => {
+    if(selectedLanguage === "English"){
+      try {
+        const response = await fetch('http://localhost:1337/api/resource?populate=deep');
+        const data = await response.json();
+        console.log(data.data.attributes)
+        setResourceData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    } else if (selectedLanguage === "French"){
+      try {
+        const response = await fetch('http://localhost:1337/api/about-page-french');
+        const data = await response.json();
+        setResourceData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    }
+    
+  };
   return (
     <div className='resources-outer-container'>
-      <div className='resources-container'>
-        <h1 className='resources-title'>Resources</h1>
-        <div className='resources-page-section'>
-          <h2 className='resources-subtitle'>Summer Camps</h2>
-          <ul className='resources-list'>
-            <li className='resources-item'>
-              <a href='https://www.ottawasummercamps.ca/' target='_blank' rel='noopener noreferrer'>Ottawa Summer Camps</a>
-            </li>
-            <li className='resources-item'>
-              <a href='https://www.ottawaparentingtimes.ca/summer-camps-guide/' target='_blank' rel='noopener noreferrer'>Ottawa Parenting Times - Summer Camps Guide</a>
-            </li>
-          </ul>
+      {resourceData ? 
+      <>
+        <div className='resources-container'>
+          <h1 className='resources-title'>{resourceData.formTitle}</h1>
+          {resourceData.ResourceGroup.map((resource, index) => (
+            <div className='resources-page-section'>
+              <h2 className='resources-subtitle'>{resource.groupTitle}</h2>
+              <ul className='resources-list'>
+                {resource.ResourceLink.map((resourcelink, index) => (
+                  <li className='resources-item'>
+                    <a href={resourcelink.link} target='_blank' rel='noopener noreferrer'>{resourcelink.linkName}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
-        <div className='resources-page-section'>
-          <h2 className='resources-subtitle'>Coding Resources</h2>
-          <ul className='resources-list'>
-            <li className='resources-item'>
-              <a href='https://code.org/' target='_blank' rel='noopener noreferrer'>Code.org</a>
-            </li>
-            <li className='resources-item'>
-              <a href='https://www.khanacademy.org/computing' target='_blank' rel='noopener noreferrer'>Khan Academy - Computing</a>
-            </li>
-            <li className='resources-item'>
-              <a href='https://scratch.mit.edu/' target='_blank' rel='noopener noreferrer'>Scratch - Imagine, Program, Share</a>
-            </li>
-          </ul>
-        </div>
-        <div className='resources-page-section'>
-          <h2 className='resources-subtitle'>Basketball</h2>
-          <ul className='resources-list'>
-            <li className='resources-item'>
-              <a href='https://www.basketball.ca/' target='_blank' rel='noopener noreferrer'>Canada Basketball</a>
-            </li>
-            <li className='resources-item'>
-              <a href='https://www.basketball.on.ca/' target='_blank' rel='noopener noreferrer'>Ontario Basketball</a>
-            </li>
-            <li className='resources-item'>
-              <a href='https://www.nba.com/canada/' target='_blank' rel='noopener noreferrer'>NBA Canada</a>
-            </li>
-          </ul>
-        </div>
-        <div className='resources-page-section'>
-          <h2 className='resources-subtitle'>Other Helpful Resources</h2>
-          <ul className='resources-list'>
-            <li className='resources-item'>
-              <a href='https://www.ottawa.ca/en' target='_blank' rel='noopener noreferrer'>City of Ottawa</a>
-            </li>
-            <li className='resources-item'>
-              <a href='https://www.ottawapublichealth.ca/en/index.aspx' target='_blank' rel='noopener noreferrer'>Ottawa Public Health</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      </> :
+      <p>Loading...</p>}
+      
     </div>
   );
 };
